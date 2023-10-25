b15bb5e7b9323e032c833be8067ed860c6b7b08d
Ersan Arik <ersan.arik@gmail.com>
2023-10-25 22:45:55 +0000

    Enabling Terraform Cloud Workspece

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       "source" = "hashicorp/azurerm"
       version  = "3.43.0"
     }
+    cloud {
+    organization = "beeops"
+
+    workspaces {
+      name = "TerraformCI"
+    }
+  }
   }
 }
 
